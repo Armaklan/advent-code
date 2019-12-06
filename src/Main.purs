65e739dd79a86ac4f9fd12b7dae3f0f2e2911d18
Lionel ZUBER <lionel-externe.zuber@enedis.fr>
2019-12-06 16:04:07 +0100

    Exo 2

--- src/Main.purs
+++ src/Main.purs
@@ -2,123 +2,51 @@ module Main where
 
 import Prelude
 
-import Data.Foldable (sum)
+import Data.Array ((!!), modifyAtIndices)
 import Data.Int (decimal, toStringAs)
+import Data.Maybe (fromMaybe)
 import Effect (Effect)
 import Effect.Console (log)
 
-masses :: Array Int
-masses = [
-120588,
-137636,
-114877,
-118328,
-97394,
-58497,
-139343,
-80307,
-125063,
-70956,
-119676,
-76115,
-91916,
-64618,
-82881,
-57000,
-141785,
-73460,
-68992,
-125701,
-97839,
-137800,
-111051,
-104591,
-114396,
-60210,
-80238,
-112009,
-70265,
-140582,
-58765,
-96848,
-130438,
-55615,
-53903,
-109361,
-129512,
-75888,
-93231,
-54697,
-125320,
-53614,
-87173,
-71762,
-147739,
-131840,
-123979,
-54434,
-121517,
-113518,
-83544,
-124924,
-76608,
-130483,
-149285,
-134147,
-111589,
-88174,
-136392,
-94448,
-139244,
-54064,
-85110,
-102985,
-95646,
-54649,
-129755,
-135795,
-119653,
-147633,
-108386,
-143180,
-126587,
-119273,
-130579,
-56006,
-83232,
-99948,
-147711,
-83092,
-99706,
-98697,
-143231,
-94526,
-53102,
-86002,
-71413,
-111054,
-147220,
-136504,
-59308,
-61123,
-148771,
-113986,
-55483,
-94426,
-62791,
-100959,
-63604,
-112511
+state :: Array Int
+state = [
+1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0
 ]
 
-computeCombustible :: Int -> Int
-computeCombustible masse
-  | masse < 6 = 0
-  | otherwise = do
-      let combustible = masse / 3 - 2
-      combustible + computeCombustible(combustible)
+extractProgram :: Array Int -> Int -> { operation:: Int, indexValue1::Int, indexValue2::Int, indexDestination::Int}
+extractProgram arr indexProgram = do
+  let debutIndexProgram = indexProgram * 4
+  {
+    operation: fromMaybe 99 (arr !! debutIndexProgram), 
+    indexValue1: fromMaybe 0 (arr !! (debutIndexProgram + 1)), 
+    indexValue2: fromMaybe 0 (arr !! (debutIndexProgram + 2)), 
+    indexDestination: fromMaybe 0 (arr !! (debutIndexProgram + 3))
+  }
+
+extractDataForProgram :: Array Int -> Int -> { operation:: Int, indexValue1::Int, indexValue2::Int, indexDestination::Int} ->  { firstVal:: Int, secondVal::Int, newIndex::Int}
+extractDataForProgram arr indexProgram program = { 
+   firstVal: fromMaybe 0 ( arr !! program.indexValue1),
+   secondVal: fromMaybe 0 ( arr !! program.indexValue2),
+   newIndex: indexProgram + 1
+  }
+
+executeOperation :: Array Int -> { operation:: Int, indexValue1::Int, indexValue2::Int, indexDestination::Int} -> Int -> ({ firstVal:: Int, secondVal::Int, newIndex::Int} -> Int) -> Int
+executeOperation arr program indexProgram operation = do
+    let pgData = extractDataForProgram arr indexProgram program
+    let newArr = modifyAtIndices [program.indexDestination] (\x -> (operation pgData)) arr
+    executeProgram newArr (extractProgram newArr pgData.newIndex) pgData.newIndex 
+
+executeProgram :: Array Int -> { operation:: Int, indexValue1::Int, indexValue2::Int, indexDestination::Int} -> Int -> Int
+executeProgram arr program indexProgram
+  | program.operation == 1 = executeOperation arr program indexProgram (\pgData -> pgData.firstVal + pgData.secondVal)
+  | program.operation == 2 = executeOperation arr program indexProgram (\pgData -> pgData.firstVal * pgData.secondVal)
+  | otherwise = fromMaybe 0 (arr !! 0)
+
+firstProgram :: Array Int -> Int
+firstProgram arr = do 
+  let newArr = modifyAtIndices [2] (\x -> 2) (modifyAtIndices [1] (\x -> 12) arr)
+  executeProgram newArr (extractProgram newArr 0) 0
 
 main :: Effect Unit
 main = do
-  let combustibles = sum (map computeCombustible masses)
-  log (toStringAs decimal combustibles)
+  log (toStringAs decimal (firstProgram state))
